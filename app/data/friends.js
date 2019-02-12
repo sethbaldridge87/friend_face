ad80868ed109d8a1ef6a3b807b6068ad811f5097
Seth Baldridge <42181695+sethbaldridge87@users.noreply.github.com>
2019-02-12 11:17:12 -0700

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-var friendsArray = [{"name":"Rey","photo":"https://lumiere-a.akamaihd.net/v1/images/talj-rey-main-image-2_668bb5dc.jpeg","scores":["3","2","4","5","2","2","3","4","2","5"]},{"name":"Luke","photo":"https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_5a38c454_461eebf5.jpeg","scores":["5","3","2","2","5","5","4","1","2","3"]},{"name":"Han","photo":"https://lumiere-a.akamaihd.net/v1/images/solo-han-solo-main_890f79bd.jpeg","scores":["1","2","5","4","3","1","2","5","3","2"]}];
+var friendsArray = [{"name":"Rey","photo":"https://lumiere-a.akamaihd.net/v1/images/talj-rey-main-image-2_668bb5dc.jpeg","scores":["2","1","5","4","5","5","2","3","3","2"]},{"name":"Luke","photo":"https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_5a38c454_461eebf5.jpeg","scores":["1","1","4","4","4","3","2","4","1","2"]},{"name":"Han","photo":"https://lumiere-a.akamaihd.net/v1/images/solo-han-solo-main_890f79bd.jpeg","scores":["4","3","3","4","2","3","2","5","5","1"]}];
 
 // exports the data found in the file
 module.exports = friendsArray;
